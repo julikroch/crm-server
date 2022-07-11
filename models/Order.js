7a34d6243317b40ad6e51b63f289b20bfeeb56ae
Julian Krochmal <juliankrochmal@gmail.com>
2022-07-11 15:50:19 -0300

    creating order model

---
+++ models/Order.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const OrderSchema = mongoose.Schema({
+    order: {
+        type: Array,
+        required: true,
+        trim: true
+    },
+    total: {
+        type: Number,
+        required: true
+    },
+    client: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'Client'
+    },
+    seller: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    },
+    state: {
+        type: String,
+        default: 'PENDING'
+    },
+    creationDate: {
+        type: Date,
+        default: Date.now()
+    }
+})
+
+module.exports = mongoose.model('Order', OrderSchema)
